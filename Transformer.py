e85695c1abc32d08709c72798994c559a9568aac
seburton <122387202+seburton@users.noreply.github.com>
2025-12-11 14:58:40 -0700

    updates to transformer

--- Transformer.py
+++ Transformer.py
@@ -6,7 +6,7 @@ from d2l import torch as d2l
 
 class MultiHeadAttention(d2l.Module):  #@save
     """Multi-head attention."""
-    def __init__(self, num_hiddens, num_heads, dropout, bias=False, **kwargs):
+    def __init__(self, num_hiddens, num_heads, dropout, num_classes=5, bias=False, **kwargs):
         super().__init__()
         self.num_heads = num_heads
         self.attention = d2l.DotProductAttention(dropout)
@@ -14,7 +14,8 @@ class MultiHeadAttention(d2l.Module):  #@save
         self.W_k = nn.LazyLinear(num_hiddens, bias=bias)
         self.W_v = nn.LazyLinear(num_hiddens, bias=bias)
         self.W_o = nn.LazyLinear(num_hiddens, bias=bias)
-
+        self.classifier = nn.Linear(num_hiddens, num_classes)
+    
     def forward(self, queries, keys, values, valid_lens):
         # Shape of queries, keys, or values:
         # (batch_size, no. of queries or key-value pairs, num_hiddens)
@@ -25,7 +26,6 @@ class MultiHeadAttention(d2l.Module):  #@save
         queries = self.transpose_qkv(self.W_q(queries))
         keys = self.transpose_qkv(self.W_k(keys))
         values = self.transpose_qkv(self.W_v(values))
-
         if valid_lens is not None:
             # On axis 0, copy the first item (scalar or vector) for num_heads
             # times, then copy the next item, and so on
@@ -35,10 +35,15 @@ class MultiHeadAttention(d2l.Module):  #@save
         # Shape of output: (batch_size * num_heads, no. of queries,
         # num_hiddens / num_heads)
         output = self.attention(queries, keys, values, valid_lens)
-        # Shape of output_concat: (batch_size, no. of queries, num_hiddens)
+        
+        #i Shape of output_concat: (batch_size, no. of queries, num_hiddens)
         output_concat = self.transpose_output(output)
-        return self.W_o(output_concat)
-
+        output_concat  = self.W_o(output_concat)
+        
+        
+        pooled = output_concat[:,0,:]
+        logits = self.classifier(pooled)        
+        return logits
 
     def transpose_qkv(self, X):
         """Transposition for parallel computation of multiple attention heads."""
@@ -55,6 +60,8 @@ class MultiHeadAttention(d2l.Module):  #@save
     
     def transpose_output(self, X):
         """Reverse the operation of transpose_qkv."""
-        X = X.reshape(-1, self.num_heads, X.shape[1], X.shape[2])
+        batch_size = X.shape[0] // self.num_heads
+        
+        X = X.reshape(batch_size, self.num_heads, X.shape[1], X.shape[2])
         X = X.permute(0, 2, 1, 3)
-        return X.reshape(X.shape[0], X.shape[1], -1)
+        return X.reshape(batch_size, X.shape[1], -1)
